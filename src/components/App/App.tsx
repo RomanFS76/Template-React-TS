ff1d4a23b0088fa0ed145511b5ac6c47347db2c7
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2025-12-06 19:27:31 +0200

    default init

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,42 +1,9 @@
-import SearchBar from '../SearchBar/SearchBar';
-import './App.module.css';
-import type { Movie } from '../../types/movie';
-import { fetchMovies } from '../../services/movieService';
-import { useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
+import css from './App.module.css';
 
 export default function App() {
-  const [movie, setMovie] = useState<Movie[]>([]);
-
-  const handleSearch = async (query: string) => {
-    const response = await fetchMovies(query);
-
-    if (response.length === 0) {
-      toast('No movies found for your request.', {
-        duration: 3000,
-        position: 'top-center',
-        style: {
-          marginTop: '100px', // ← отступ сверху
-          fontSize: '18px', // ← больше текст
-          padding: '16px 20px',
-          fontWeight: '700', // ← больше размер сообщения
-          color: 'black', // цвет текста
-          background: '#d1c64d', // цвет фона
-          border: '2px solid black', // цвет границы
-          borderRadius: '8px', // скругление углов
-        },
-      });
-    }
-
-    setMovie(response);
-
-    console.log(movie);
-  };
-
   return (
     <>
-      <SearchBar onSubmit={handleSearch} />
-      <Toaster />
+      <div className={css.app}>Hello</div>
     </>
   );
 }
--- src/components/Loader/Loader.tsx
+++
@@ -1,5 +0,0 @@
-import './App.module.css';
-
-export default function App() {
-  return <div>hello</div>;
-}
